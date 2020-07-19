89abccff003c51acd886a096a82005613cef5554
Siro <siro.knubel@gmail.com>
2020-07-19 04:24:27 +0200

    test

---
+++ iNeedHelp/iNeedHelp/Assets/flyfuck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class flyfuck : MonoBehaviour
+{
+    public float meow = 1;
+    private Rigidbody2D rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * meow;
+        }
+    }
+}
--- iNeedHelp/iNeedHelp/Assets/mainmenu.cs
+++ iNeedHelp/iNeedHelp/Assets/mainmenu.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class mainmenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void PlayTheFuckingGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Game");
     }
 }
